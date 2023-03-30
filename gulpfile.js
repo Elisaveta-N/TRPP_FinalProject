934d45ac7f28ca30d5490ae4b307b051c654e72f
Lisa <liza.nickonova@mail.ru>
2023-03-30 10:34:25 +0300

    Доработала gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const{src, dest, parallel, series, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
-
+const clean = require('gulp-clean');
 
 function browsersync(){
     browserSync.init({
@@ -25,8 +25,25 @@ function scripts(){
 
 function startwatch(){
     watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+    watch('app/**/*.css').on('change', browserSync.reload);
+    watch('app/**/*.html').on('change', browserSync.reload);
+}
+
+function buildcopy() {
+	return src([ // Выбираем нужные файлы
+		'app/**/*.css',
+		'app/js/**/*.min.js',
+		'app/img/**/*',
+		'app/**/*.html',
+		], { base: 'app' }) // Параметр "base" сохраняет структуру проекта при копировании
+	.pipe(dest('dist')) // Выгружаем в папку с финальной сборкой
+}
+
+function cleandist() {
+	return src('dist', {allowEmpty: true}).pipe(clean()) // Удаляем папку "dist/"
 }
 
 exports.browsersync = browsersync;
 exports.scripts = scripts;
+exports.build = series(cleandist, scripts, buildcopy);
 exports.default = parallel(scripts, browsersync, startwatch);
